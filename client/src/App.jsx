94f86b914ea4bf266917431335733b6c07f083a4
wolfy-six <mendis.seneth@gmail.com>
2024-04-18 00:39:55 +0530

    small changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
         <Route path="/addblog" element={<AddBlog />} />
         <Route path="/deleteblog" element={<DeleteBlog />} />
         <Route path="/profile" element={<Profile />} />
+        {/* <Route path="/profile" element={<Profile />} /> */}
       </Routes>
     </>
   );
